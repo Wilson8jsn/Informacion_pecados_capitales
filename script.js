679f4605a23373b5a89f2a6a8a4176db162473a5
Wilson8jsn <dc105677543@gmail.com>
2023-06-26 21:01:41 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,76 @@
+function showInfo(sin) {
+  let info = '';
+
+  switch (sin) {
+    case 'Soberbia':
+      info = '<img src="img/infierno.jpg" alt="Soberbia" style="border-radius: 10px;;width: 300px; height: 200px;">' +
+        '<p>La soberbia, considerado el mayor pecado capital y el origen de todos los demás, se presenta cuando alguien sobrevalora ante sí y ante los demás su propio ego, su propia persona, cuando siente o piensa que es una persona mejor, con mayores méritos, atributos y capacidades que los demás que lo rodean. Y, por lo tanto, obra despreciándolos. Por esa razón, una persona soberbia tiene el gran defecto o limitación en su personalidad de no reconocer las cualidades y méritos de los otros, el valor que tienen. Al no reconocerles su valor, se convierte en un ser incapaz de tratarlos como sus semejantes iguales..</p>';
+      break;
+    case 'Avaricia':
+      info = '<img src="img/fox sin of greed tattoo from seven deadly sins.jpg" alt="Avaricia" style="border-radius: 10px;width: 300px; height: 200px; ">' +
+        '<p>La avaricia, por su parte, nace de no gastar o usar el dinero que se posee en exceso para atender necesidades de personas con las que tienen lazos familiares o afectivos, o en complacer algunos de sus deseos razonables y válidos. El avaro no solo es aquel que guarda o atesora el dinero que posee, que no le hace falta o que no necesita. El avaro, sobre todo, se niega a compartirlo con los familiares o amigos que lo necesitan..</p>';
+       '<p> Así, a pesar de la riqueza monetaria que posee, el avaro se arruina a sí mismo como ser humano, pierde la riqueza espiritual de existencia que brota siempre de la posibilidad de compartir lo que se posee con los necesitados. Por eso Dante los encuentra en el purgatorio “echados y atados al suelo”, al piso físico y material que les impide elevarse al universo espiritual donde podrán encontrar la luz de Dios..</p>';
+      break;
+    case 'Envidia':
+      info = '<img src="img/222.png" alt="Envidia" style="border-radius: 10px; width: 300px; height: 200px">' +
+        '<p>Sentir envidia es un pecado porque quienes la sienten desean en su interior que los bienes que tienen o viven otros desaparezcan; desean con fuerza que pierdan esos bienes para sentirse contentos o satisfechos. De tal manera que son seres humanos que desean el mal al prójimo, los que anhelan que otros pierdan los bienes que tienen y disfrutan, y, que, por lo tanto, sufran por ese motivo. Al desear el mal los convierte en gran medida malos o pecadores..</p>';
+      break;
+    case 'Ira':
+      info = '<img src="img/111.jpg" alt="Ira" style="border-radius: 10px; width: 300px; height: 200px;">' +
+        '<p>Una emoción considerada pecaminosa porque hace aflorar en quien la sufre el odio o deseo de muerte de otra u otras personas. Dante sitúa a los que han pecado de ira en la segunda grada del purgatorio y la define como: “El amor que alguien siente por la justicia que, sin embargo, lo pervierte en deseo de venganza y en resentimiento”. Para él, la ira surge en una persona cuando ha sufrido un daño o una afrenta de otra y, cuando siente que esta no ha sido sancionada o reparada justamente, convierte esa frustración en odio o resentimiento. El sentir que no se ha hecho justicia lleva al odio a quien sufrió; y ese odio no es otra cosa que una ira permanente que atrapa poderosamente..</p>';
+      break;
+    case 'Pereza':
+      info = '<img src="img/~♛.jpg" alt="Pereza" style="border-radius: 10px; width: 150px; heigth:150;">' +
+        '<p>La pereza también es un pecado en la medida en que el perezoso renuncia o se niega a obrar usando el mayor atributo natural o innato que tiene como ser humano, su libertad o su libre albedrío. Así, el perezoso comete una falta contra esta cualidad que naturalmente posee, es decir, niega la mayor virtud que la naturaleza le dio.</p>';
+      break;
+    case 'Gula':
+      info = '<img src="img/0006.png" alt="Gula" style="border-radius: 10px; width :190px; height:230px;">' +
+        '<p>La gula es un pecado en tanto los golosos comen y beben sin mesura. No lo hace para satisfacer la necesidad natural de consumirlos para preservar sus vidas, sino por el puro placer que les provoca este consumo. Cuando los hombres comen y beben en exceso incurren en una falta de desmesura, violan la medida racional que la naturaleza les ordena seguir en los actos de sus vidas. Dice Dante al encontrar a los golosos en la sexta grada del purgatorio.</p>';
+      break;
+    case 'Lujuria':
+      info = '<img src="img/lujuria.png" alt="Lujuria" style="border-radius: 10px; width: 150px; heigth:200;">' +
+        '<p>La lujuria, por su parte, es el pecado que cometen quienes quedan atrapados por un excesivo deseo sexual al que no le ponen límites, o al que no quieren o no pueden controlar. Dante los encuentra sufriendo en el segundo círculo del infierno. Pues cuando los hombres viven sometidos a satisfacer deseos sexuales desenfrenados cometen una falta grave, quedar presos en la “cárcel” de la carne que los aleja de la naturaleza espiritual que poseen, que es su verdadera naturaleza humana. Dejan de ser ellos mismos al rebajar o enajenar sus existencias humano-espirituales al nivel del puro instinto.</p>';
+      break;
+   
+      case 'Luna':
+        info = '<img src="img/luna.jpg" alt="Luna" style="border-radius: 10px;;width: 150px; heigth: 150px;">' +
+          '<p>El tema principal que se aborda en esta esfera trata sobre los cumplimientos de los votos y la importancia de la fidelidad a las promesas hechas..</p>';
+        break;
+      case 'Mercurio':
+        info = '<img src="img/Mercurio.jpg" alt="Mercurio" style="border-radius: 10px;width: 300px; height: 200px; ">' +
+          '<p>En esta esfera, se explora del conocimiento de la sabiduría y el conocimiento en la vida espiritual </p>';
+        '</P> espiritual..</p>;'
+    
+        break;
+      case 'Venus':
+        info = '<img src="img/VENUS.jpg" alt="Venus" style="border-radius: 10px; width: 300px; height: 200px">' +
+          '<p>La tercera esfera habla del amor, tanto en la forma terrenal como en la forma como en su manifestación divina, puede conducir a la elevación espiritual y a la unión con lo divino. Se enfatiza la idea de que el amor auténtico y desinteresado puede ser una fuerza poderosa y redentora en el camino hacia la trascendencia espiritual...</p>';
+        break;
+      case 'EL SOL':
+        info = '<img src="img/sol.jpg" alt="EL SOL" style="border-radius: 10px; width: 300px; height: 200px;">' +
+          '<p>En el Cielo del Sol, Dante se encuentra con las almas de los sabios y los teólogos, como San Buenaventura y Tomás de Aquino. Estas almas irradiantes revelan la sabiduría divina y comparten conocimientos profundos sobre la naturaleza de Dios y su creación...</p>';
+          '<p>En esta esfera, se exploran conceptos como la iluminación espiritual, la verdad absoluta y la comprensión divina. Dante comprende la perfección y la armonía que emanan de la luz divina y cómo esto conduce a una comprensión más profunda de la existencia y el propósito divino...</p>';
+        break;
+      case 'MARTE':
+        info = '<img src="img/marte.jpg" alt="MARTE" style="border-radius: 10px; width: 150px; heigth:150;">' +
+          '<p>Este cielo donde se encuentran las almas de los guerreros que han demostrado valentía y han llevado actos heroicos en el nombre de la fe, estas almas se revelan como luces brillantes y forman una gran cruz en el cielo. Dante describe a los espíritus que habitan en esta esfera como llenos de una ferviente energía y dedicación a la causa celestial, él se encuentra con varios personajes como Julio César, Josué y Judas Macabeo a través de diálogos se explora temas relacionados con la justicia, virtud y la guerra justa.</p>';
+        break;
+      case 'JUPITER':
+        info = '<img src="img/jupiter.jpg" alt="JUPITER" style="border-radius: 10px; width :190px; height:230px;">' +
+          '<p>Se encuentran las almas de los gobernadores justos y sabios como David. Estas brillantes almas forman una magnifica corona alrededor de la estrella.</p>';
+        break;
+      case 'SATURNO':
+        info = '<img src="img/saturno.jpg" alt="SATURNO" style="border-radius: 10px; width: 150px; heigth:200;">' +
+          '<p>Esta esfera mantiene a todos los filósofos que en la vida terrenal fueron en búsqueda de sabiduría y la contemplación divina.</p>';
+        break;
+        case 'ESTRELLAS FIJAS':
+        info = '<img src="img/estrellas.png" alt="ESTRELLAS FIJAS" style="border-radius: 10px; width: 150px; heigth:200;">' +
+          '<p>Cuando Dante asciende a través de las esferas celestiales, encuentra a las almas de los justos y los bienaventurados en cada una de ellas. Estas almas se convierten en "estrellas" que brillan con una luz radiante y que forman patrones y figuras celestiales, como cruces y coronas. Estas representaciones simbólicas de las almas justas en el cielo se asemejan a las estrellas fijas y sirven como símbolos de la perfección divina y la armonía celestial.</p>';
+        break;
+      default:
+        info = '<p>Seleccione un pecado para obtener información.</p>';
+        break;
+  }
+
+  document.getElementById('sinInfo').innerHTML = info;
+}
